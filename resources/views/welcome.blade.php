a32ec0c2e1f00feba5a0a4415f9b62ceaa22d856
Emmanuel <kasumba904@gmail.com>
2021-06-23 22:18:36 +0300

    With all files needed for the project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css"/>
+    <title>HMS</title>
+</head>
+  <body>
+    <div class="container" >
+      <div class="sidebar">
+         <img src="/Images/Logo-2.jpg" width="400" >
+      </div>
+      <div class="sidebar2">
+          <form>
+              <fieldset>
+                  <legend>WELCOME TO HMS</legend>
+                   <label>Sign In </label><br><br>
+                   <input id="sign" type="text"><br>
+                   <label>Password</label><br><br>
+                   <input id="pass" type="password"><br>
+                   <button id="btn">SUBMIT</button>
+              </fieldset>
+          </form>
+      </div>    
+     
+    </div>
+  </body>
+</html>
