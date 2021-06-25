0cce1d7fbeba0bb818be8fe55d4a7034d079785c
Emmanuel <kasumba904@gmail.com>
2021-06-25 14:14:27 +0300

    html

---
+++ resources/views/page.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css"/>
+    <title>HMS</title>
+</head>
+  <body>
+    <div class="container" >
+      <div class="sidebar">
+         <img src="/Images/Logo-2.jpg" width="400" >
+      </div>
+      <div class="sidebar2">
+          
+      </div>    
+     
+    </div>
+  </body>
+</html>
